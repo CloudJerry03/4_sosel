f5e9f9dfe6d380a1e03c863fe85ed4c3f0c4a817
CloudJerry03 <gomdalp1701@gmail.com>
2020-08-17 00:44:56 +0900

    some

--- JssProject/main/views.py
+++ JssProject/main/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import JssForm
+from .models import Jasoseol
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    all_jss = Jasoseol.objects.all()
+    return render(request, 'index.html', {'all_jss' : all_jss})
 
 def create(request):
+    if request.method == "POST":#POST일 경우 실행
+        filed_form = JssForm(request.POST)
+        if filed_form.is_valid():#유효성 검증
+            filed_form.save()#object를 저장
+            return redirect('index')
+
     jss_form = JssForm()
     return render(request, 'create.html', {'jss_form' : jss_form})
